ef215ec4c7252e06d85f5ec6152aa225265fd7b2
HyeongCheol Seo <wjdqh6544@naver.com>
2024-05-18 19:06:38 +0900

    Edit class structure to save position of each group for each Member. (To correspond multiple group of Each Member)
    <New> MemberPosition
    - Save Member's position of Each group
    - [Variable] groupName, position
    - [Method] getter, setter, constructor
    
    <Edit> Member
    - [Remove variable] Position
    - [Edit Variable; ArrayList<String> -> ArrayList<MemberPosition>] groupList
    (It save "Group: Position" pairs of each member)

--- src/Entity/Member.java
+++ src/Entity/Member.java
@@ -11,22 +11,19 @@ public class Member {
     private String name;
     private Date birthday;
     private String email;
-    private PositionList position;
-    private ArrayList<String> groupList;
+    private ArrayList<MemberPosition> groupList;
     public Member(){ // default Constructor
         setName("None");
         setBirthday(new Date());
         setEmail("None");
-        setPosition(PositionList.NONE);
-        setGroupList(new ArrayList<String>());
+        setGroupList(new ArrayList<MemberPosition>());
     }
-    public Member(String name, int year, int month, int day, String email, PositionList position, String... inputGroupList) { // Constructor using Information
+    public Member(String name, int year, int month, int day, String email, MemberPosition... inputGroupList) { // Constructor with Parameters
         setName(name);
         setBirthday(new Date(year, month, day));
         setEmail(email);
-        setPosition(position);
-        setGroupList(new ArrayList<String>());
-        for (String group : inputGroupList) {
+        setGroupList(new ArrayList<MemberPosition>());
+        for (MemberPosition group : inputGroupList) {
             addGroup(group);
         }
     }
@@ -34,12 +31,11 @@ public class Member {
         setName(otherObj.getName());
         setBirthday(otherObj.getBirthday()); // Copied Date Obj
         setEmail(otherObj.getEmail());
-        setPosition(otherObj.getPosition());
         setGroupList(otherObj.getGroupList()); // Copied ArrayList
     }
-    public ArrayList<String> cloneArrList(ArrayList<String> originalArrList) { // ArrayList Deep Copy
-        ArrayList<String> newArr = new ArrayList<String>();
-        for (String obj : originalArrList) {
+    public ArrayList<MemberPosition> cloneArrList(ArrayList<MemberPosition> originalArrList) { // ArrayList Deep Copy
+        ArrayList<MemberPosition> newArr = new ArrayList<MemberPosition>();
+        for (MemberPosition obj : originalArrList) {
             newArr.add(obj);
         }
         return newArr;
@@ -47,12 +43,10 @@ public class Member {
     public String getName(){ return this.name; }
     public Date getBirthday() {return new Date(this.birthday); }
     public String getEmail() { return this.email; }
-    public PositionList getPosition() { return this.position; }
-    public ArrayList<String> getGroupList() { return cloneArrList(groupList); }
+    public ArrayList<MemberPosition> getGroupList() { return cloneArrList(groupList); }
     private void setName(String name) { this.name = name; }
     private void setBirthday(Date birthday) { this.birthday = new Date(birthday); }
     private void setEmail(String email) { this.email = email; }
-    private void setPosition(PositionList position) { this.position = position; }
-    private void setGroupList (ArrayList<String> groupList) { this.groupList = groupList; }
-    public void addGroup(String groupName) { this.groupList.add(groupName); }
+    private void setGroupList (ArrayList<MemberPosition> groupList) { this.groupList = groupList; }
+    public void addGroup(MemberPosition groupName) { this.groupList.add(groupName); }
 }
---
+++ src/Entity/MemberPosition.java
@@ -0,0 +1,31 @@
+package Entity;
+
+import javafx.geometry.Pos;
+
+/*
+ * COMP217: Java Programming / Team 03
+ * Prof: Suh, Young-Kyoon
+ *
+ * MemberPosition: class For member's position of each Group.
+ * @author: Seo, HyeongCheol
+ */
+public class MemberPosition {
+    private String groupName;
+    private PositionList position;
+    public MemberPosition() { // default constructor
+        setGroupName("None");
+        setPosition(PositionList.NONE);
+    }
+    public MemberPosition(String name, PositionList position){ // Constructor with Parameters
+        setGroupName(name);
+        setPosition(position);
+    }
+    public MemberPosition(MemberPosition otherObj){ // Copy Constructor
+        setGroupName(otherObj.getGroupName());
+        setPosition(otherObj.getPosition());
+    }
+    public String getGroupName(){ return this.groupName; }
+    public PositionList getPosition() { return this.position; }
+    private void setGroupName(String groupName) { this.groupName = groupName; }
+    private void setPosition(PositionList position) { this.position = position; }
+}
