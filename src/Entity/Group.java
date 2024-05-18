6116ad7d01e9164a06ac02c80b9df9659eae9ca3
HyeongCheol Seo <wjdqh6544@naver.com>
2024-05-18 18:34:10 +0900

    Add new instance variable (in Entity) - Group
    Add an ArrayList for saving Transaction History.
    (This ArrayList contains each Transaction Object.)

--- src/Entity/Group.java
+++ src/Entity/Group.java
@@ -10,9 +10,11 @@ import java.util.ArrayList;
 public class Group implements cloneArrayList{
     private String groupName;
     private ArrayList<Member> groupMemberList;
+    private ArrayList<Transaction> transactionList;
     public Group() { // default constructor
         setGroupName("None");
         setMemberList(new ArrayList<Member>());
+        setTransactionList(new ArrayList<Transaction>());
     }
     public Group(String name, Member... memberObjList) { // Constructor with parameter
         setGroupName(name);
@@ -20,10 +22,12 @@ public class Group implements cloneArrayList{
         for (Member memberObj : memberObjList) {
             addMember(new Member(memberObj));
         }
+        setTransactionList(new ArrayList<Transaction>());
     }
     public Group(Group otherObj){ // copy constructor
         setGroupName(otherObj.getGroupName());
         setMemberList(otherObj.getMemberList()); // Copied ArrayList
+        setTransactionList(otherObj.getTransactionList()); // Copied ArrayList
     }
     public ArrayList<Member> cloneArrList(ArrayList<Member> original){
         ArrayList<Member> newArr = new ArrayList<Member>();
@@ -32,9 +36,21 @@ public class Group implements cloneArrayList{
         }
         return newArr;
     }
+    public ArrayList<Transaction> cloneTransList(ArrayList<Transaction> original){
+        ArrayList<Transaction> newArr = new ArrayList<Transaction>();
+        for (Transaction transaction : original) {
+            newArr.add(new Transaction(transaction));
+        }
+        return newArr;
+    }
     public String getGroupName() { return this.groupName; }
     public ArrayList<Member> getMemberList() { return cloneArrList(this.groupMemberList); }
+
+    public ArrayList<Transaction> getTransactionList() { return cloneTransList(this.transactionList); }
+
     private void setGroupName(String name) { this.groupName = name; }
     private void setMemberList(ArrayList<Member> groupMemberList) { this.groupMemberList = groupMemberList; }
+    private void setTransactionList(ArrayList<Transaction> transactionList) { this.transactionList = transactionList; }
     public void addMember(Member obj) { this.groupMemberList.add(obj); }
+    public void addTransaction(Transaction obj) { this.transactionList.add(obj); }
 }
