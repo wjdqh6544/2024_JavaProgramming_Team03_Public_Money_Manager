d48c1206488d5be7bfe96190103bea964009b521
HyeongCheol Seo <wjdqh6544@naver.com>
2024-05-26 23:36:47 +0900

    Add methods to TransactionService class.
    [Add]
    - addFinishMember: Add member that finished the settlement.
    - getAllFinishMember: return a list of member that finished the settlement.
    - getAllMember: return a list of member that is subject to settlement.

--- src/Service/TransactionService.java
+++ src/Service/TransactionService.java
@@ -1,6 +1,9 @@
 package Service;
 
 import Entity.Transaction;
+import Entity.abs_Member;
+
+import java.util.ArrayList;
 
 /*
  * COMP217: Java Programming / Team 03
@@ -11,6 +14,15 @@ import Entity.Transaction;
  */
 public class TransactionService {
     private Transaction transaction;
+    public void addFinishMember(abs_Member obj){
+        transaction.addFinishMember(obj);
+    }
+    public ArrayList<abs_Member> getAllFinishMember() {
+        return transaction.getFinishMember();
+    }
+    public ArrayList<abs_Member> getAllMember(){
+        return transaction.getMemberList();
+    }
     public int getExpensePerPeople(Transaction transObj) {
         return Math.round(transObj.getExpense() / transObj.getMemberList().size());
     }
